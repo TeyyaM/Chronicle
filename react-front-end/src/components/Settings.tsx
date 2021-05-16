8670b0093c61ef7aa64511ffc685602884495a33
Tessa Azar MacLean <tessaamac@gmail.com>
2021-05-16 01:58:29 +0000

    Add 5 nonfunctional colour pickers

--- react-front-end/src/components/Settings.tsx
+++ react-front-end/src/components/Settings.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
-
+import { useContext, useState } from 'react'; 
+import { UserContext } from '../hooks/UserContext';  
 
 
 export default function Settings() {
   // const history = useHistory();
   // const [isLoading, setIsLoading] = useState(false);
-  const [color, setColor] = useState('#aabbcc');
+  const { userRef } = useContext(UserContext);   
+  const user = userRef.current;
+  const [background, setBackground] = useState(user ? user.background_hex : '#76c2f5');
+  const [secondary, setSecondary] = useState(user ? user.secondary_hex : '#0B3C5D');
+  const [accent, setAccent] = useState(user ? user.accent_hex : '#ebb30e');
+  const [text, setText] = useState(user ? user.text_hex : '#fafafa');
+  const [title, setTitle] = useState(user ? user.titlehex : '#1D2731');
+
 
   return (
     <div className="Settings">
-      <HexColorPicker color={color} onChange={setColor} />
+      <HexColorPicker color={background} onChange={setBackground} />
+      <HexColorPicker color={secondary} onChange={setSecondary} />
+      <HexColorPicker color={accent} onChange={setAccent} />
+      <HexColorPicker color={text} onChange={setText} />
+      <HexColorPicker color={title} onChange={setTitle} />
     </div>
   );
 }
