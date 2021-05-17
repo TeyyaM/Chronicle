a8ac0d921b9d518224174a58c6ac86f6cecc956a
Josiah <josiah.pilon@yahoo.ca>
2021-05-16 19:15:11 -0700

    Refactored function that displays an emoji if the entry has one

--- react-front-end/src/components/Entry.tsx
+++ react-front-end/src/components/Entry.tsx
@@ -8,11 +8,27 @@ import { UserContext } from '../hooks/UserContext';
 import { smiley, mild, neutral, unhappy, angry } from './emojis';
 
 interface Params {entryId: string};
+interface Data {
+  title: string;
+  content: string;
+  privacy: boolean;
+  category_id: number | string | null;
+  date_created: Date | null;
+  mood: number | string | null;
+};
 
 const Entry = () => {
-
   const { userRef } = useContext(UserContext);
   const user = userRef.current;
+  const [ editMode, setEditMode ] = useState<boolean>(false)
+  const [ content, setContent ] = useState<Data>({
+    title: "",
+    content: "",
+    mood: 0,
+    privacy: true,
+    date_created: null,
+    category_id: null
+  });
   
   const entryStyling = {
     backgroundColor: user ? user.background_hex : '#0b3c5d',
@@ -25,30 +41,8 @@ const Entry = () => {
     borderRadius: 10
   }  
   
-  interface Data {
-    title: string;
-    content: string;
-    privacy: boolean;
-    category_id: number | string | null;
-    date_created: Date | null;
-    mood: number | string| null;
-  }
-
-  const [ content, setContent ] = useState<Data>({
-    title: "",
-    content: "",
-    mood: 0,
-    privacy: true,
-    date_created: null,
-    category_id: null
-  });
-  const [ editMode, setEditMode ] = useState<boolean>(false)
-
-  
   const params: Params = useParams();
-
   useEffect(() => {
-    // console.log("useEffect is called")
     axios.get(`/api/entries/${params.entryId}`)
       .then(res => setContent({...res.data[0]})); // Do stuff with it
   }, [params.entryId]);
@@ -58,46 +52,39 @@ const Entry = () => {
   // }
   // may have to update route
 
-      // displays mood icon
-      const moodImage = (num: number | string) => {
-        const imgs = {
-          1: {
-            src: angry,
-            name: 'Very Unhappy'
-          },
-          2: {
-            src: unhappy,
-            name: 'Unhappy'
-          },
-          3: {
-            src: neutral,
-            name: 'Neutral'
-          },
-          4: {
-            src: mild,
-            name: 'Happy'
-          },
-          5: {
-            src: smiley,
-            name: 'Very Happy'
-            }
-        };
-        return imgs[num];
-      }
-
-  // const history = useHistory();
-  // console.log(history);
-
-  const availableMood = () => {
-
-    if(content.mood) {
-      const mood = moodImage(content.mood);
-      return <p><img src={mood.src} alt={mood.name} /></p> 
+  // displays emoji if entry has a mood
+  function moodImage (num: number | string | null) {
+    if (num) {
+      const imgs = {
+        1: {
+          src: angry,
+          name: 'Very Unhappy'
+        },
+        2: {
+          src: unhappy,
+          name: 'Unhappy'
+        },
+        3: {
+          src: neutral,
+          name: 'Neutral'
+        },
+        4: {
+          src: mild,
+          name: 'Happy'
+        },
+        5: {
+          src: smiley,
+          name: 'Very Happy'
+          }
+      };
+        return (
+        <p><img src={imgs[num].src} alt={imgs[num].name}/></p>
+        );
     } else {
-     return null;
-    }
-  }
+      return null;
+    }}
 
+  
   function titleHandler(event) {
     setContent(prev => ({...prev, title: event.target.value}))
   };
@@ -106,7 +93,7 @@ const Entry = () => {
     setContent((prev) => ({...prev, content: event.target.value}))
   };
 
-  let buttonMessage = editMode ? "Save" : "Edit";
+  const buttonMessage = editMode ? "Save" : "Edit";
 
   return (
     <Fragment>
@@ -137,7 +124,7 @@ const Entry = () => {
       <h2 >{content.title}</h2>
       <p>{content.date_created}</p>
       <p>{content.privacy}</p>
-      {availableMood()}
+      {moodImage(content.mood)}
       <p>{content.content}</p>
     </div>)}
       <button onClick={() => setEditMode(true)}>{buttonMessage}</button>
